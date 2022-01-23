69f006bd2e00def55788a6c6b38f0e06121e370d
unknown <allansichangi35@gmail.com>
2022-01-23 13:45:13 +0300

    memoized fibonnaci

---
+++ Memoization/fib-memo.js
@@ -0,0 +1,12 @@
+function fibonnaci(n, memo = {}) {
+	if (n in memo) {
+		return memo[n];
+	}
+	if (n == 0 || n == 1) {
+		return n;
+	}
+	memo[n] = fibonnaci(n - 1, memo) + fibonnaci(n - 2, memo);
+	return memo[n];
+}
+
+console.log(fibonnaci(6));
