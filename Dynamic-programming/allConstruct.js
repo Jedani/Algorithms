87718c09cc01798e2520fec5c51a504e724537d9
unknown <allansichangi35@gmail.com>
2022-01-24 11:57:23 +0300

    dynamic

--- Dynamic-programming/allConstruct.js
+++ Dynamic-programming/allConstruct.js
@@ -1,6 +1,19 @@
 const allConstruct = (target, wordbank, memo = {}) => {
 	if (target in memo) return memo[target];
-	if (target == "") return 1;
+	if (target == "") return [[]];
+	const result = [];
+
+	for (let word of wordbank) {
+		if (target.indexOf(word) == 0) {
+			const suffix = target.slice(word.length);
+			const suffWays = allConstruct(suffix, wordbank, memo);
+			const targetWays = suffWays.map((way) => [word, ...way]);
+			result.push(...targetWays);
+		}
+	}
+
+	memo[target] = result;
+	return result;
 };
 
 console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
