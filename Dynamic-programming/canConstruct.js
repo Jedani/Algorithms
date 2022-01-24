8fba875710ceaa146c34c7ac769b456a79953815
unknown <allansichangi35@gmail.com>
2022-01-24 10:40:43 +0300

    dynamic

---
+++ Dynamic-programming/canConstruct.js
@@ -0,0 +1,26 @@
+const canConstruct = (target, wordbank, memo = {}) => {
+	if (target in memo) return memo[target];
+	if (target == "") return true;
+
+	for (let word of wordbank) {
+		if (target.indexOf(word) == 0) {
+			const suffix = target.slice(word.length);
+			if (canConstruct(suffix, wordbank, memo) == true) {
+				memo[target] = true;
+				return true;
+			}
+		}
+	}
+	memo[target] = false;
+
+	return false;
+};
+
+console.log(canConstruct("allan", ["a", "n", "l"]));
+console.log(
+	canConstruct(
+		"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeg",
+
+		["e", "ee", "eee", "eeee", "eeeeee"],
+	),
+);
