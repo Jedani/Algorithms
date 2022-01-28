581ffd8a06e56d526e05c29de8da2249a6edbcfa
unknown <allansichangi35@gmail.com>
2022-01-28 17:36:09 +0300

    leet quiz

--- Dynamic-programming/leetcode/twoSum.js
+++ Dynamic-programming/leetcode/twoSum.js
@@ -1,5 +1,6 @@
 const twoSum = (target, nums) => {
 	let store = {};
+
 	for (let [index, num] of nums.entries()) {
 		if (store[num] !== undefined) {
 			return [store[num], index];
@@ -8,6 +9,6 @@ const twoSum = (target, nums) => {
 	}
 };
 
-console.log(twoSum(9, [2, 11, 15, 7]));
-console.log(twoSum(6, [3, 3]));
-console.log(twoSum(6, [3, 2, 4]));
+// // console.log(twoSum(9, [2, 11, 15, 7]));
+// // console.log(twoSum(6, [3, 3]));
+// console.log(twoSum(6, [3, 2, 4]));
