f7fdc6dd7f04f105519e99b17654d29eb31147fd
unknown <allansichangi35@gmail.com>
2022-02-01 15:23:14 +0300

    leet quiz

---
+++ Dynamic-programming/leetcode/matrixReshape.js
@@ -0,0 +1,18 @@
+const matrixReshape = (mat, r, c) => {
+	
+    const flat = mat.flat()
+    if (flat.length !== r*c) return mat;
+    return [...Array(r)].map(() => flat.splice(0,c)) 
+
+};
+
+console.log(
+	matrixReshape(
+		[
+			[2, 3],
+			[3, 4],
+		],
+		1,
+		4,
+	),
+);
