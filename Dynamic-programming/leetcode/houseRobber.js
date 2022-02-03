f56a9237e04d513f4825fa637a6413e0590927da
unknown <allansichangi35@gmail.com>
2022-02-03 23:08:10 +0300

    leet quiz

--- Dynamic-programming/leetcode/houseRobber.js
+++ Dynamic-programming/leetcode/houseRobber.js
@@ -9,5 +9,26 @@ const rob = function (nums) {
 	return Math.max(nums[nums.length - 1], nums[nums.length - 2]);
 };
 
+// OR recursivley
+
+var rob = function (nums) {
+	if (nums.length == 0) return 0;
+	if (nums.length == 1) return nums[0];
+
+	const robHouse = (start, end) => {
+		let prev = 0,
+			curr = 0;
+
+		//prev means house at i - 2, curr is house at i - 1
+		for (let i = start; i < end; i++) {
+			let temp = Math.max(nums[i] + prev, curr);
+			prev = curr;
+			curr = temp;
+		}
+		return curr;
+	};
+	return Math.max(robHouse(0, nums.length), robHouse(0, nums.length));
+};
+
 console.log(rob([1, 2, 3, 1]));
 console.log(rob([2, 7, 9, 3, 1]));
