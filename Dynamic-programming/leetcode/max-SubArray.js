c043398f7cbde295dbdb6b9376c56474d54ee83c
unknown <allansichangi35@gmail.com>
2022-02-04 23:00:19 +0300

    leet quiz

--- Dynamic-programming/leetcode/max-SubArray.js
+++ Dynamic-programming/leetcode/max-SubArray.js
@@ -1,13 +1,40 @@
+// const maxSubArray = (nums) => {
+// 	let maxSub = nums[0];
+// 	let currSum = 0;
+// 	for (let num of nums) {
+// 		if (currSum < 0) {
+// 			currSum = 0;
+// 		}
+// 		currSum += num;
+// 		maxSub = Math.max(maxSub, currSum);
+// 	}
+// 	return maxSub;
+// };
+// maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+
+// OR finding with indices using Kadanes algorithm
+
 const maxSubArray = (nums) => {
-	let maxSub = nums[0];
-	let currSum = 0;
-	for (let num of nums) {
-		if (currSum < 0) {
-			currSum = 0;
+	let currMax = nums[0];
+	let endMax = 0;
+
+	let start = 0;
+	end = 0;
+	s = 0;
+	for (let i = 0; i < nums.length; i++) {
+		endMax = endMax + nums[i];
+		if (currMax < endMax) {
+			currMax = endMax;
+			start = s;
+			end = i;
 		}
-		currSum += num;
-		maxSub = Math.max(maxSub, currSum);
+		if (endMax < 0) {
+			endMax = 0;
+			s = i + 1;
+		}
 	}
-	return maxSub;
+	let ans = `max value is ${currMax} and its indexes are [ ${start},${end} ]`;
+	return ans;
 };
-maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
--- Peak&Valley/peakValleyfinding.js
+++ Peak&Valley/peakValleyfinding.js
@@ -1,5 +1,8 @@
 const peakValley = (arr) => {
-	arr.sort();
+	arr.sort(function (a, b) {
+		return a - b;
+	});
+	console.log(arr);
 	function swap(array, a, b) {
 		let temp = array[a];
 		array[a] = array[b];
@@ -12,4 +15,4 @@ const peakValley = (arr) => {
 	return arr;
 };
 
-console.log(peakValley([5, 6, 0, 3, 2, 9]));
+console.log(peakValley([5, 6, 0, 3, 2, 9, 3]));
