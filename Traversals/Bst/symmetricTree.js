04118bba84528bfb1ed7db5922036b76530d9a4b
unknown <allansichangi35@gmail.com>
2022-02-08 23:02:24 +0300

    traversal in trees

--- Traversals/Bst/symmetricTree.js
+++ Traversals/Bst/symmetricTree.js
@@ -1,3 +1,18 @@
-var isSymmetric = function (root) {};
+var isSymmetric = function (root) {
+	if (root == null || check(root.left, root.right)) return true;
+
+	function check(leftSub, rightSub) {
+		if (leftSub == null && rightSub == null) return true;
+
+		if (leftSub !== null && rightSub !== null) {
+			return (
+				leftSub.val == rightSub.val &&
+				check(leftSub.left, rightSub.right) &&
+				check(leftSub.right, rightSub.left)
+			);
+		}
+	}
+	return false;
+};
 
 console.log(isSymmetric([1, 2, 2, 3, 4, 4, 3]));
