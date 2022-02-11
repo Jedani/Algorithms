25d0fbc583da54012d1cfed5336aa93643655d3f
Allan <sichangiallan1@gmail.com>
2022-02-11 04:51:21 +0300

    leet quiz

---
+++ Dynamic-programming/leetcode/uniqueBsts.js
@@ -0,0 +1,18 @@
+var numTrees = function (n) {
+	let mem = Array(n + 1);
+	(mem[1] = 1), (mem[2] = 2);
+	function f(n) {
+		if (n <= 1) return 1;
+		if (n == 2) return 2;
+		if (mem[n]) return mem[n];
+		let ans = 0;
+		for (let i = 0; i < n; i++) {
+			ans = ans + f(i) * f(n - 1 - i);
+		}
+		mem[n] = ans;
+		return ans;
+	}
+	return f(n);
+};
+
+console.log(numTrees(3));
