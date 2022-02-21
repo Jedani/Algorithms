2219e2141ee9f766a83b7b8a9046490a85d90f3c
Allan <allansichangi35@gmail.com>
2022-02-21 11:39:47 +0300

    leet quiz

---
+++ Dynamic-programming/leetcode/increasingTripletSubseq.js
@@ -0,0 +1,24 @@
+var increasingTriplet = function (nums) {
+	if (nums.length < 3) {
+		return false;
+	}
+
+	let q = +Infinity;
+	let j = +Infinity;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] <= q) {
+			q = nums[i];
+		} else if (nums[i] <= j) {
+			j = nums[i];
+		} else {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+console.log(increasingTriplet([2, 1, 5, 0, 4, 6]));
+
+// OR SLIDNG WINDOW TECHNIQUE
