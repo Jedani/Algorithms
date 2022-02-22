4cd6df0b0ba872b71c0f5715aed47e7b9d26c9db
Allan <allansichangi35@gmail.com>
2022-02-22 10:23:56 +0300

    leet quiz

--- Dynamic-programming/leetcode/prod-array-exceptSelf.js
+++ Dynamic-programming/leetcode/prod-array-exceptSelf.js
@@ -1,3 +1,39 @@
-var productExceptSelf = function (nums) {};
+var productExceptSelf = function (nums) {
+	let output = Array();
+	if (nums.length < 1) {
+		return output;
+	}
+	let product = 1;
+	for (let i = 0; i < nums.length; i++) {
+		product *= nums[i];
+		output.push(product);
+	}
 
+	product = 1;
+	for (let i = nums.length - 1; i > 0; i--) {
+		output[i] = output[i - 1] * product;
+		product *= nums[i];
+	}
+	output[0] = product;
+	return output;
+};
+
+var productExceptSelf = function (nums) {
+	let n = nums.length;
+	let result = new Array(n).fill(1);
+	let prefixProduct = 1;
+	let suffixProduct = 1;
+	let left = 0;
+	let right = n - 1;
+
+	while (left < n) {
+		result[left] *= prefixProduct;
+		result[right] *= suffixProduct;
+		prefixProduct *= nums[left++];
+		suffixProduct *= nums[right--];
+	}
+	return result;
+};
+
+// console.log(productExceptSelf([-1, 1, 0, -3, 3]));
 console.log(productExceptSelf([1, 2, 3, 4]));
